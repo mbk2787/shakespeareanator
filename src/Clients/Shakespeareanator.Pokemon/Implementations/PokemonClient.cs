c07e2a9205c8928e4582ad4ef8e45d43577ac9ca
Marco Bernasconi <bernasconi.berna@gmail.com>
2020-06-09 22:33:05 +0200

    Replaced formatted string with combined url

--- src/Clients/Shakespeareanator.Pokemon/Implementations/PokemonClient.cs
+++ src/Clients/Shakespeareanator.Pokemon/Implementations/PokemonClient.cs
@@ -1,21 +1,29 @@
 ﻿namespace Shakespeareanator.Pokemon
 {
+    using Flurl;
     using Flurl.Http;
     using Shakespeareanator.Pokemon.Interfaces;
     using System.Threading.Tasks;
 
     public class PokemonClient : IPokemonClient
     {
+        #region Constants
+
+        private const string POKE_API_URL = "https://pokeapi.co/api/v2";
+        private const string POKE_API_SPECIES = "pokemon-species";
+
+        #endregion
+
         #region Pokemon Species
 
         public async Task<PokemonSpeciesDto> GetPokemonSpecieByName(string name)
         {
-            return await "https://pokeapi.co/api/v2/pokemon-species".GetJsonAsync<PokemonSpeciesDto>();
+            return await Url.Combine(POKE_API_URL, POKE_API_SPECIES, name).GetJsonAsync<PokemonSpeciesDto>();
         }
 
         public async Task<PokemonSpeciesDto> GetPokemonSpecieById(long id)
         {
-            return await "https://pokeapi.co/api/v2/pokemon-species".GetJsonAsync<PokemonSpeciesDto>();
+            return await Url.Combine(POKE_API_URL, POKE_API_SPECIES, id.ToString()).GetJsonAsync<PokemonSpeciesDto>();
         }
 
         #endregion
